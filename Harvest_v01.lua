b75786330471aab3f057b411cbe26a69647c5641
HKragh <k_r_a_g_h@yahoo.dk>
2016-11-06 19:38:19 +0100

    Add files via upload

--- Harvest_v01.lua
+++ Harvest_v01.lua
@@ -1,6 +1,6 @@
 local c = peripheral.wrap("left")
 local axisCount = 7
-
+local harvestPause = 30
 local iAxisMod = 0
 local jAxisMod = 0
 
@@ -26,75 +26,94 @@ function moveToPickupStart()
   turtle.turnRight()
   moveForward(axisCount)
   turtle.turnLeft()
-  iAxisMod = 1  
+end
 
+function moveToHarvestStart()
+
+  moveBack(axisCount)
+  turtle.turnRight()
+  moveForward(axisCount-1)
+  turtle.turnLeft()
 end
 
 function harvestMove()
 
-for i = 1, axisCount+1 do
+	for i = 1, axisCount+1 do
   
-  if ((iAxisMod == 0) and (i<axisMod)) or (iAxisMod == 1) then
+	  if ((iAxisMod == 0) and (i<axisCount+1)) or (iAxisMod == 1) then
 
-   for j = 1, axisCount do
+	   for j = 1, axisCount do
    
-   
+		if (iAxisMod == 1) and (j == 1) then
+			turtle.suckDown()
+		end
       
-      if ((i == 4) and (j < axisCount)) or (i ~= 4) then
+		  if ((i == 4+iAxisMod) and (j < axisCount)) or (i ~= 4+iAxisMod) then
       
-        if (i == 4) and (j == 4) then
-          turtle.turnRight()
-          moveForward(1)
-          turtle.turnLeft()
-          moveForward(2)
-          turtle.turnLeft()
-          moveForward(1)
-          turtle.turnRight()
+			if (i == 4+iAxisMod) and (j == 4) then
+			  turtle.turnRight()
+			  moveForward(1)
+			  turtle.turnLeft()
+			  moveForward(2)
+			  turtle.turnLeft()
+			  moveForward(1)
+			  turtle.turnRight()
       
-        else
-          moveForward(1)       
-        end
-        --c.clickDown()
-        
-      end
+			else
+			  moveForward(1)       
+			end
+			if iAxisMod == 0 then
+				c.clickDown()
+			elseif iAxisMod == 1 then
+				turtle.suckDown()
+
+			end
+			
+		  end
                   
-   end
+	   end
    
-   if i == 4 then
-     axisSubtract = 0
-   end   
+	   if i == 4+iAxisMod then
+		 turtle.turnLeft()
+		 moveForward(1)
+		 turtle.turnRight()
    
-   if i == 4 then
-     turtle.turnLeft()
-     moveForward(1)
-     turtle.turnRight()
+		 moveBack(axisCount)
+	   else
    
-     for k = 1, axisCount do
-     
-       turtle.back()  
-     end
-   else
-   
-     if i ~= axisCount then   
-       for k = 1, axisCount do
-   
-          turtle.back()
-   
-       end
+		 if i ~= axisCount+iAxisMod then   
+		   
+		   moveBack(axisCount)
           
-       turtle.turnLeft()
-       moveForward(1)
-       turtle.turnRight()
-     else
-        if iAxisMod == 0 then        
-          moveToPickupStart()  
-        end     
-     end
-   end
-   --Loop End
-   end
-end
+		   turtle.turnLeft()
+		   moveForward(1)
+		   turtle.turnRight()
+		 else
+			if iAxisMod == 0 then        
+			  moveToPickupStart()  
+			  do return end
+			elseif iAxisMod == 1 then
+				moveToHarvestStart()  
+				do return end
+			end     
+		 end
+	   end
+	   --Loop End
+	   end
+	end
 
 end
 
-harvestMove()
+while true do
+	harvestMove()
+	iAxisMod = 1 
+	harvestMove()
+
+  for i = 1, 16 do
+    turtle.select(i)
+    turtle.dropDown()  
+  end
+
+	sleep(harvestPause)
+	iAxisMod = 0 
+end
